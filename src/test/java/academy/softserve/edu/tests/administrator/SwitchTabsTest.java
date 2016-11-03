267ca4e6ec47ca7c8069f45bbde68990eaa2d97d
Nikodych <shkliar.dmytro@gmail.com>
2016-11-03 10:51:37 +0200

    SwitchTabsTest navigates into oms site

--- src/test/java/academy/softserve/edu/tests/administrator/SwitchTabsTest.java
+++ src/test/java/academy/softserve/edu/tests/administrator/SwitchTabsTest.java
@@ -23,38 +23,38 @@ public class SwitchTabsTest /*extends TestRunner*/ {
 //***************************************************************
 
 
-    public static final String HOME_URL = PropertiesReader.getDefaultProperty("login.url");
-//    public static final String PATH_TO_CHROME_DRIVER = PropertiesReader.getDefaultProperty("path.webdriver.chrome.linux");
-    public static final int TIMEOUT = 30;
+//    public static final String HOME_URL = PropertiesReader.getDefaultProperty("login.url");
+////    public static final String PATH_TO_CHROME_DRIVER = PropertiesReader.getDefaultProperty("path.webdriver.chrome.linux");
+//    public static final int TIMEOUT = 30;
+//
+//    @Getter
+//    protected WebDriver driver;
+//
+//    protected UserInfoPage userInfoPage;
+//    protected LogInPage logInPage;
+//    protected AdministrationPage administrationPage;
 
-    @Getter
-    protected WebDriver driver;
 
-    protected UserInfoPage userInfoPage;
-    protected LogInPage logInPage;
-    protected AdministrationPage administrationPage;
+//    @BeforeMethod
+//    public final void setUp() {
+//
+//        System.setProperty("webdriver.chrome.driver", /*PATH_TO_CHROME_DRIVER*/"/usr/bin/chromedriver");
+//
+//        driver = new ChromeDriver();
+//
+//        Dimension dimension = new Dimension(1920, 1080);
+//
+//        driver.manage()
+//                .window()
+//                .setSize(dimension);
+//
+//        driver.manage().timeouts().implicitlyWait(TIMEOUT, TimeUnit.SECONDS);
+//
+//        driver.get(HOME_URL);
+//
+//        logInPage = new LogInPage(driver);
 
-
-    @BeforeMethod
-    public final void setUp() {
-
-        System.setProperty("webdriver.chrome.driver", /*PATH_TO_CHROME_DRIVER*/"/usr/bin/chromedriver");
-
-        driver = new ChromeDriver();
-
-        Dimension dimension = new Dimension(1920, 1080);
-
-        driver.manage()
-                .window()
-                .setSize(dimension);
-
-        driver.manage().timeouts().implicitlyWait(TIMEOUT, TimeUnit.SECONDS);
-
-        driver.get(HOME_URL);
-
-        logInPage = new LogInPage(driver);
-
-    }
+//    }
 
 
 //***************************************************************
@@ -66,42 +66,48 @@ public class SwitchTabsTest /*extends TestRunner*/ {
     @Test
     final public void testSwitchingBetweenTabs() {
 
-        userInfoPage = logInPage.logInAs(Roles.ADMINISTRATOR);
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://52.37.0.203:8080/oms5/");
+        System.out.println("Chrome browser opened and navigated to OMS site");
+        driver.quit();
 
-        assertThat(userInfoPage.getUserInfoFieldSet())
-                .isDisplayed();
-
-        assertThat(userInfoPage.getUserInfoLink())
-                .isDisplayed();
-
-        assertThat(userInfoPage.getAdministrationLink())
-                .isDisplayed();
-
-        administrationPage = userInfoPage.goToAdministrationPage();
-
-        assertThat(administrationPage.getUserInfoLink())
-                .isDisplayed();
-
-        assertThat(administrationPage.getAdministrationLink())
-                .isDisplayed();
-
-        administrationPage.goToUserInfoPage();
-
-        assertThat(userInfoPage.getUserInfoLink())
-                .isDisplayed();
-
-        assertThat(userInfoPage.getAdministrationLink())
-                .isDisplayed();
-
-        userInfoPage.doLogOut();
+//        userInfoPage = logInPage.logInAs(Roles.ADMINISTRATOR);
+//
+//        assertThat(userInfoPage.getUserInfoFieldSet())
+//                .isDisplayed();
+//
+//        assertThat(userInfoPage.getUserInfoLink())
+//                .isDisplayed();
+//
+//        assertThat(userInfoPage.getAdministrationLink())
+//                .isDisplayed();
+//
+//        administrationPage = userInfoPage.goToAdministrationPage();
+//
+//        assertThat(administrationPage.getUserInfoLink())
+//                .isDisplayed();
+//
+//        assertThat(administrationPage.getAdministrationLink())
+//                .isDisplayed();
+//
+//        administrationPage.goToUserInfoPage();
+//
+//        assertThat(userInfoPage.getUserInfoLink())
+//                .isDisplayed();
+//
+//        assertThat(userInfoPage.getAdministrationLink())
+//                .isDisplayed();
+//
+//        userInfoPage.doLogOut();
 
     }
 
 //*******************************************************************
-    @AfterMethod
-    public final void tearDown() {
-
-        driver.quit();
-    }
+//    @AfterMethod
+//    public final void tearDown() {
+//
+//        driver.quit();
+//    }
 //*******************************************************************
 }
